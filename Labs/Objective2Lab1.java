87c02c641dc203dd438e5391b1f7d554f275c234
ieshiak <ieshiak@icloud.com>
2023-11-08 14:18:32 -0500

    Complete Objective2Lab1

---
+++ Labs/Objective2Lab1.java
@@ -0,0 +1,8 @@
+public class Objective2Lab1 {
+	public static void main(String[] args) {
+		
+		String hello; //declaringn the variable named "hello"
+		hello = "Hello World"; //inititalizing the variable named "hello"
+		System.out.println(hello);
+	}
+}
