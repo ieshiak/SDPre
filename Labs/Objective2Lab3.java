b35e03e85791f4389dbd19690e440551b5c1e8c1
ieshiak <ieshiak@icloud.com>
2023-11-08 14:11:36 -0500

    Complete Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,12 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    int YearOfBirth = 1986;
+    String MonthOfBirth = "July";
+    Boolean isThisWeekend = false;
+
+  System.out.println(YearOfBirth);
+  System.out.println(MonthOfBirth);
+  System.out.println(isThisWeekend);
+
+  }
+}
