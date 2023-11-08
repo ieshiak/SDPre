cbb51f94367ae7b77c67174da05a89dea08f4acf
ieshiak <ieshiak@icloud.com>
2023-11-08 17:41:51 -0500

    complete Objective3Lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,10 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    int num1 = 15;
+    int num2 = 6;
+    int num3 = 2;
+    double sum = num1 + num2 + num3;
+    double average = sum / 3;
+    System.out.println(average);
+	}
+}
