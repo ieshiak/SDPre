992e3465a688d529b49e586bae4b014865cf18c3
ieshiak <ieshiak@icloud.com>
2023-11-08 20:48:51 -0500

    working on Objective3Lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,8 @@
+public class Objective3Lab2 {
+    public static void main(String[] args) {
+        double side1 = 10.0;
+        double side2 = 8.0;
+        double sum = 
+        double hypotenuse = (side1 * 2) * (side2 * 2);
+    
+    System.out.println(The hypotenuse of a triangle with sides (side1) and (side2 is (hypotenuse));
