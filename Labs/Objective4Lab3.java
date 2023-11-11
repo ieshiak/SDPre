5a998b5ca5dea06df60ee2204ac594307325dc1d
ieshiak <ieshiak@icloud.com>
2023-11-11 12:06:26 -0500

    complete Objective4Lab3

---
+++ Labs/Objective4Lab3.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class Objective4Lab3 {
+  public static void main(String[] args) {
+    
+    int currentYear = 2023;
+    Scanner input = new Scanner(System.in);
+    System.out.println("How old are you?");
+    int age = input.nextInt();
+    int birthYear = currentYear - age;
+    System.out.println("You were born in " + birthYear);
+  }
+}
