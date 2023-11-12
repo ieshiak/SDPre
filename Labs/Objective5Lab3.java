7c3464b2f471fa0a97bd9dc1bc9b687377160d7f
ieshiak <ieshiak@icloud.com>
2023-11-11 19:41:19 -0500

    completed Objective5Lab3

--- Labs/Objective5Lab3.java
+++ Labs/Objective5Lab3.java
@@ -1,22 +1,19 @@
+import java.util.Scanner;
 public class Objective5Lab3 {
     public static void main(String[] args) {
-      
-    Scanner input = new Scanner(System.in);
+      Scanner keyboard = new Scanner(System.in);
+      System.out.println("Please enter a number: ");
+      int num1 = keyboard.nextInt();
 
-    System.out.println("Please enter a number: ");
-      int userNum = input.nextInt;
-
-    if(userNum > 0) {
+    if(num1 > 0) {
       System.out.println("The number is positive");
     }
-    if(userNum < 0) {
+    if(num1 < 0) {
       System.out.println("The number is negative");
     }
-    if(userNum == 0) {
+    if(num1 == 0) {
       System.out.println("The number equals to 0");
     }
-
-    Scanner.close();
-  
+    
   }
 }
