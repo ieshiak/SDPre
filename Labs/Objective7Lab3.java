ce7b564b090af4d43e94985f96775bd52191fe7e
ieshiak <ieshiak@icloud.com>
2023-11-12 23:39:44 -0500

    completed Objective7Lab3

--- Labs/Objective7Lab3.java
+++ Labs/Objective7Lab3.java
@@ -2,18 +2,14 @@ public class Objective7Lab3 {
   public static void main(String[] args) {
     int counter = 1;
 
-   while(counter < 20) {
-      System.out.println(counter);
-      counter = counter + 1;
-      }
+   while(counter <= 20) {
       if(counter % 2 == 0){
         System.out.println(counter + " even ");
-    
       }
       else{
         System.out.println(counter + " odd ");
-        
-
     }
+    counter++;
   }
+}
 }
